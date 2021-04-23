a93d4939953b42d1db1020873400f6f390cbfb36
Dylan Player <dylan@dylanplayer.com>
2021-04-23 04:14:41 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// Require Libraries
+const express = require('express');
+// Require tenorjs near the top of the file
+  const Tenor = require("tenorjs").client({
+    // Replace with your own key
+    "Key": "HZKSDH5D2MKV", // https://tenor.com/developer/keyregistration
+    "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+    "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
+
+
+// App Setup
+const app = express();
+
+
+
+//Tell app to use public folder
+app.use(express.static('public'));
+
+
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
+
+
+// Routes
+  app.get('/', (req, res) => {
+    // Handle the home page when we haven't queried yet
+    term = ""
+    if (req.query.term) {
+        term = req.query.term
+    }
+    // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+    Tenor.Search.Query(term, "20")
+        .then(response => {
+            // store the gifs we get back from the search
+            const gifs = response;
+            // pass the gifs as an object into the home page
+            res.render('home', { gifs })
+        }).catch(console.error);
+  })
+
+
+
+
+// Start Server
+app.listen(3000, () => {
+  console.log('Gif Search listening on port localhost:3000!');
+});
